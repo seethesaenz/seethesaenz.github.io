a01b71d250c4b620f7fea866e080cfe535eb227e
Jackson Saenz <saenzjack1@icloud.com>
2024-06-19 19:42:36 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Projects from './components/Projects';
 import About from './components/About';
-import './App.css';
+import Projects from './components/Projects';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/about">About Me</Link></li>
-          </ul>
-        </nav>
-        <Router>
-          <Route exact path="/" component={Home} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/about" component={About} />
-        </Router>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
     </Router>
   );
-};
+}
 
 export default App;
